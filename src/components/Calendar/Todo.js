3102fea68e8af367ea484e423b7d19129829417e
Mariana  Serrão <marianaserrao@Marianas-MacBook-Air.local>
2023-03-21 16:12:12 +0100

    todos.js added edit and remove icon buttons to work with functionality created

--- src/components/Calendar/Todo.js
+++ src/components/Calendar/Todo.js
@@ -1,21 +1,42 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
-import {} icon from 
-import {} icon from 
+import {RiCloseCircleLine} icon from 'react-icons/ri'
+import {TiEdit} icon from "react-icons/ti"
 
-function Todo() {
-  return (
+function Todo({todos, completeTodo, removeTodo, updateTodo}) {
+
+    const [edit, setEdit] = useState ({
+        id:null, 
+        value: ''
+    });
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value)
+        setEdit ({
+            id:null,
+            value: ''
+        });
+    }; 
+
+    if (edit.id) {
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />;
+    }
+
+  return todos.map((todo, index) => (
     <div className={Todo.isComplete ? 'todo-row complete' : 'todo-row'}
     key={index}>
-        <div key={todo.id} onClick = {()=> completeTodo(todo.id)}>{todo.text}
+        <div key={todo.id} onClick ={() => completeTodo(todo.id)}>{todo.text}
         </div>
         <div className='icons'>
-            
+            <RiCloseCircleLine
+            onClick={() => removeTodo(todo.id)}
+            className= 'delete-icon'/>
+            <TiEdit onClick={() => setEdit({id: todo.id, value:todo.text})}
+            className= 'edit-icon'/>
         </div>
-        
         </div>
-  );
+  ));
 };
 
 export default Todo
